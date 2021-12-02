ae4a28d82b56fca520c7dc9f05bf9f7d40d5feaf
ryanc2112 <30004700@live.nwrc.ac.uk>
2021-12-02 13:49:59 +0000

    partial view added

--- LibrarySite/LibrarySite/Controllers/HomeController.cs
+++ LibrarySite/LibrarySite/Controllers/HomeController.cs
@@ -20,6 +20,7 @@ namespace LibrarySite.Controllers
             return View();
         }
 
+        
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
---
+++ LibrarySite/LibrarySite/Controllers/ManagePartialViewController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibrarySite.Controllers
+{
+    public class ManagePartialViewController : Controller
+    {
+        // GET: ManagePartialView
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated == true)
+            {
+                return PartialView("_MenuDetailed");
+            }
+            else
+            {
+                return PartialView("_MenuBasic");
+            }
+
+        }
+
+    }
+}
